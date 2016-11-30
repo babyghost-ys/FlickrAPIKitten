0bea14901c3bf1fb0852449ebc3e8c1a4abd8166
Peter Leung <babyghost@gmail.com>
2016-11-30 20:29:47 +0000

    Basic class setup including CustomCell and Posts object.

---
+++ FlickrAPIKitten/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  FlickrAPIKitten
+//
+//  Created by Peter Leung on 30/11/2016.
+//  Copyright © 2016 winandmac Media. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+    @IBOutlet weak var kittenImageView: UIImageView!
+    
+    func configureCell(inputThumbURL: String){
+        
+    }
+}
---
+++ FlickrAPIKitten/Posts.swift
@@ -0,0 +1,17 @@
+//
+//  Posts.swift
+//  FlickrAPIKitten
+//
+//  Created by Peter Leung on 30/11/2016.
+//  Copyright © 2016 winandmac Media. All rights reserved.
+//
+
+import Foundation
+
+class Posts {
+    var thumbImageLink:String!
+    
+    init(inputImgLink: String) {
+        self.thumbImageLink = inputImgLink
+    }
+}
